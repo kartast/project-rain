466f3e3864d50b4438f344b534d4f2ae5ec1f805
karta <karta87@gmail.com>
2012-10-02 21:27:33 +0800

    Initial Commit

---
+++ levelHelperHello/AppDelegate.h
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.h
+//  levelHelperHello
+//
+//  Created by karta on 2/10/12.
+//  Copyright karta 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "cocos2d.h"
+
+@interface AppController : NSObject <UIApplicationDelegate, CCDirectorDelegate>
+{
+	UIWindow *window_;
+	UINavigationController *navController_;
+	
+	CCDirectorIOS	*director_;							// weak ref
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (readonly) UINavigationController *navController;
+@property (readonly) CCDirectorIOS *director;
+
+@end
---
+++ levelHelperHello/IntroLayer.h
@@ -0,0 +1,21 @@
+//
+//  IntroLayer.h
+//  levelHelperHello
+//
+//  Created by karta on 2/10/12.
+//  Copyright karta 2012. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface IntroLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
