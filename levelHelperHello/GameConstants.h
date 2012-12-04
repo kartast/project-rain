a7e0e4570a3da9151a7efdc56c7969a2613c8257
karta <karta87@gmail.com>
2012-12-04 20:01:54 +0800

    new level

--- levelHelperHello/GameConstants.h
+++ levelHelperHello/GameConstants.h
@@ -10,8 +10,8 @@
 #define levelHelperHello_GameConstants_h
 
 // Goal frame, from sad -> neutral -> happy
-//
-static int GOAL_FAIL_FRAME_INDEX    = 0;
+//static int GOAL_FAIL_FRAME_INDEX    = 0;
+ 
 static int GOAL_NEUTRAL_FRAME_INDEX = 4;
 static int GOAL_SUCCESS_FRAME_INDEX = 8;
 
@@ -20,6 +20,7 @@ static const float gravityMult = 0.6;
 static const float spawnVelMax = 7.5;
 static const float fAmplifyMult = 0.02;
 static const float fDampMult = 0.6;
+
 #define GOAL_COLOR          @"goal_color"
 #define GOAL_COLOR_YELLOW   @"goal_color_yellow"
 #define GOAL_COLOR_RED      @"goal_color_red"
